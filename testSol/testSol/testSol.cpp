ed3ed1ddb3f0e593f88e62135f92e48e80c6f0ad
mattlacorte <mlacorte@uncc.edu>
2017-04-10 18:18:31 -0400

    test solution. Please ignore

---
+++ testSol/testSol/testSol.cpp
@@ -0,0 +1,16 @@
+// testSol.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+	cout << "Weenie" << endl;
+
+    return 0;
+}
+
