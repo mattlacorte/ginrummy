3d1a0b8996c451f11bbbb3ac0d8aea3ea874ed56
mattlacorte <mlacorte@uncc.edu>
2017-04-11 15:42:28 -0400

    Update ginRummy.cpp

--- ginRummy.cpp
+++ ginRummy.cpp
@@ -1,6 +1,6 @@
 /* ginRummy.cpp : Defines the entry point for the console application.
  *
- * Matt LaCorte, Alrfred Joseph, Jacob Pasley
+ * Matt LaCorte, Alfred Joseph, Jacob Pasley
  *
  * ECGR 2104 Gin Rummy Final
  *
@@ -9,19 +9,11 @@
 #include "stdafx.h"
 #include <iostream>
 
+#include "functions.h"
+
 using namespace std;
 
-class Card {
-private:
-	int suit;		//will store suit type as int. 1 = hearts, 2 = diamonds, 3 = spades, 4 = clubs
-	int face;		//stores face of card as int. 1 = A, 2 = 2, 3 = 3, ..., 11 = J, 12 = Q, 13 = K
-	int val;		//stores value of card. A = 1, 2 = 2, ..., JQK = 10
-public:
-	//sort function
-	//deadwood check
-	//knock check
-	//gin check
-};
+
 
 int main()
 {
@@ -32,6 +24,12 @@ int main()
 
 
 	//run function to decide who deals		-- MATT
+		//must initialize and shuffle cards first
+			//cards stored in vector of type CARD
+			
+		//ask user to pick 1-52
+		//comp picks 1-52 excluding user choice
+		//compare cards
 
 
 	//deal cards							-- ALFRED
